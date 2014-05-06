0dcb884cd46d6c8881b8965943131c6219816276
eblondel <emmanuel.blondel1@gmail.com>
2014-05-06 18:41:05 +0200

    code formating

--- R/plot2map.R
+++ R/plot2map.R
@@ -1,19 +1,18 @@
 #	Rmapit - plot2map.R		
 #	Copyright (C) 2014 - Emmanuel Blondel
 #
-#	This program is free software; you can redistribute it and/or modify
-#	it under the terms of the GNU General Public License as published by
-#	the Free Software Foundation; either version 2 of the License, or
-#	(at your option) any later version.
+#	This program is free software; you can redistribute it and/or modify it under 
+# the terms of the GNU General Public License as published by the Free Software 
+# Foundation; either version 2 of the License, or (at your option) any later 
+# version.
 
-#	This program is distributed in the hope that it will be useful,
-#	but WITHOUT ANY WARRANTY; without even the implied warranty of
-#	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#	GNU General Public License for more details.
+#	This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS 
+# FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
 
-#	You should have received a copy of the GNU General Public License along
-#	with this program; if not, write to the Free Software Foundation, Inc.,
-#	51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+#	You should have received a copy of the GNU General Public License along with 
+# this program; if not, write to the Free Software Foundation, Inc., 51 Franklin
+# Street, Fifth Floor, Boston, MA 02110-1301 USA.
 #
 #	Contact: emmanuel.blondel1 (at) gmail.com
 
@@ -124,12 +123,12 @@ plot2map <- function(sp, sp.ref, stat, stat.ref, stat.handler, ratio = 0.05,
     
     #handle the stat subset
     substat <- switch(class(stat),
-                "data.frame" = stat[as.character(stat[,stat.ref]) ==
-                                          as.character(subsp@data[1,sp.ref]),],
-                "list" = if(!is.null(names(stat)))
-                           stat[[as.character(subsp@data[1,sp.ref])]]
-                         else
-                           stat[[1]]
+                      "data.frame" = stat[as.character(stat[,stat.ref]) ==
+                                            as.character(subsp@data[1,sp.ref]),],
+                      "list" = if(!is.null(names(stat)))
+                        stat[[as.character(subsp@data[1,sp.ref])]]
+                      else
+                        stat[[1]]
     )
     
     if(nrow(substat) > 0){
